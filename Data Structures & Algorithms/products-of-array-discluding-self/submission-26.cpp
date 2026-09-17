d6f5f143543e95c7b1c3fdcd091885fa3492b34d
Coloma Andrei <131942315+003Andrei@users.noreply.github.com>
2026-09-17 05:35:06 -0900

    Add: products-of-array-discluding-self - submission-26

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-26.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n, 1);
+        
+        // Pass 1: Calculate the prefix products and store them in result
+        int prefix = 1;
+        for (int i = 0; i < n; i++) {
+            result[i] = prefix;
+            prefix *= nums[i];
+        }
+        
+        // Pass 2: Calculate suffix products on the fly and multiply with prefix
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] *= suffix;
+            suffix *= nums[i];
+        }
+        
+        return result;
+    }
+};
